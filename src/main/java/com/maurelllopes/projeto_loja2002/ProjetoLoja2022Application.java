b81f2db8f27b94111d8175a0e408f62dfe19b4e8
Alexandre Maurell <maurelllopes@mac.com>
2021-09-11 09:42:30 -0300

    Projeto Criado com Banco H2

---
+++ src/main/java/com/maurelllopes/projeto_loja2002/ProjetoLoja2022Application.java
@@ -0,0 +1,13 @@
+package com.maurelllopes.projeto_loja2002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLoja2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLoja2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/maurelllopes/projeto_loja2002/ProjetoLoja2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maurelllopes.projeto_loja2002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLoja2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
