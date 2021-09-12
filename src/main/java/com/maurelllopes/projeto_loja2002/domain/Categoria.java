00a97c1b7ee5a11a2fd6bf4d44c51b3891c0977d
Alexandre Maurell <maurelllopes@mac.com>
2021-09-12 03:23:52 -0300

    PRODUTO E ASSOCIACAO MUITOS PARA MUITOS

--- src/main/java/com/maurelllopes/projeto_loja2002/domain/Categoria.java
+++ src/main/java/com/maurelllopes/projeto_loja2002/domain/Categoria.java
@@ -1,11 +1,15 @@
 package com.maurelllopes.projeto_loja2002.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -14,6 +18,9 @@ public class Categoria implements Serializable {
 		@GeneratedValue(strategy=GenerationType.IDENTITY)
 		private Integer id;
 		private String nome;
+		
+		@ManyToMany(mappedBy="categorias")
+		private List<Produto> produtos = new ArrayList<>();
 
 		public Categoria() {
 			
@@ -24,38 +31,6 @@ public class Categoria implements Serializable {
 			this.id = id;
 			this.nome = nome;
 		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((id == null) ? 0 : id.hashCode());
-			result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Categoria other = (Categoria) obj;
-			if (id == null) {
-				if (other.id != null)
-					return false;
-			} else if (!id.equals(other.id))
-				return false;
-			if (nome == null) {
-				if (other.nome != null)
-					return false;
-			} else if (!nome.equals(other.nome))
-				return false;
-			return true;
-		}
-
 		public Integer getId() {
 			return id;
 		}
@@ -72,5 +47,29 @@ public class Categoria implements Serializable {
 			this.nome = nome;
 		}
 
+		public List<Produto> getProdutos() {
+			return produtos;
+		}
+
+		public void setProdutos(List<Produto> produtos) {
+			this.produtos = produtos;
+		}
+
+
+		   @Override
+		    public boolean equals(Object o) {
+		        if (this == o) return true;
+		       if (o == null || getClass() != o.getClass()) return false;
+		       Categoria categoria = (Categoria) o;
+		       return Objects.equals(id, categoria.id);
+		   }
+
+		    @Override
+		    public int hashCode() {
+
+		        return Objects.hash(id);
+		    }
+
+	
 		
 	}
---
+++ src/main/java/com/maurelllopes/projeto_loja2002/repositories/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.maurelllopes.projeto_loja2002.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.maurelllopes.projeto_loja2002.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
+
+
