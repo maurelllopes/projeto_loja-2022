5cba3c31d502b95826f377a331df1eccfb3eda2b
Alexandre Maurell <maurelllopes@mac.com>
2021-09-12 11:51:04 -0300

    ESTADO E CIDADE

---
+++ src/main/java/com/maurelllopes/projeto_loja2002/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package com.maurelllopes.projeto_loja2002.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.maurelllopes.projeto_loja2002.domain.Cidade;
+	
+	@Repository
+	public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+	}
+
