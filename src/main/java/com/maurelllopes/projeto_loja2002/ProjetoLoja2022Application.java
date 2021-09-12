fa92714df35abde2f9a898dd84ac0da1db3fc21a
Alexandre Maurell <maurelllopes@mac.com>
2021-09-12 02:01:02 -0300

    CRIANDO OPERAÇÃO DE INSTANCIAÇÃO COM @AUTOWIRED

--- src/main/java/com/maurelllopes/projeto_loja2002/ProjetoLoja2022Application.java
+++ src/main/java/com/maurelllopes/projeto_loja2002/ProjetoLoja2022Application.java
@@ -1,13 +1,32 @@
 package com.maurelllopes.projeto_loja2002;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.maurelllopes.projeto_loja2002.domain.Categoria;
+import com.maurelllopes.projeto_loja2002.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetoLoja2022Application {
+public class ProjetoLoja2022Application implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoLoja2022Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		 
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
